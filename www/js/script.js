6e2f47b8d337014e796f3c77cb23cebebca46736
satoshi okami <me.after.12am@gmail.com>
2012-04-13 05:15:34 +0900

    gui updated. now animation available.

--- www/js/script.js
+++ www/js/script.js
@@ -1,18 +1,52 @@
 
-var fps = 20, theta = 0;
-var ui, glitchParam;
+var fps = 10, theta = 0;
+var glitchGui, glitchParam;
 var canvas;
 var texture;
 var glitch;
-var video, videoCanvas;
+var video;
+
+
+/*  extend dat.gui
+    get controller by name
+-------------------------------------------------*/
+dat.GUI.prototype.get = function(property_name) {
+	var c;
+	this.__controllers.forEach(function(d) {
+		if (d.property == property_name) {
+			c = d;
+			return;
+		}
+	});
+	for (key in this.__folders) {
+		this.__folders[key].__controllers.forEach(function(d) {
+			if (d.property == property_name) {
+				c = d;
+				return;
+			}
+		});
+	}
+	return c;
+};
+
+function clamp(lo, value, hi) {
+	
+	return Math.max(lo, Math.min(value, hi));
+}
 
 function init() {
 	
-	if (texture) texture.destroy();
+	if (texture) {
+	    texture.destroy();
+	}
 	texture = null;
+	
+	if ($('#canvas video').size() > 0) {
+	    $('#canvas video').remove();
+	}
+	video = null;
+	
 	glitch = null;
-	videoCanvas = null;
-	$('#canvas video').remove();
 };
 
 function loadImage(src) {
@@ -23,11 +57,9 @@ function loadImage(src) {
 	image.onload = function(e) {
 		
 		$('#drop-zone').hide();
-		//$('#canvas img').remove();
-		//$('#canvas').append($(image));
-		
 		$(canvas).css('margin-top', - image.height / 2);
 		$(canvas).css('margin-left', - image.width / 2);
+		$(canvas).show();
 		
 		texture = canvas.texture(image);
 		glitch = canvas.glitch(texture);
@@ -45,56 +77,102 @@ function loadVideo(src) {
 		$('#canvas').append($(video));
 		$(video).hide();
 		
-		videoCanvas = document.createElement('canvas');
+		var videoCanvas = document.createElement('canvas');
 		videoCanvas.width = $(video).width();
 		videoCanvas.height = $(video).height();
 		
+		video.videoCanvas = videoCanvas;
 		texture = canvas.texture(video);
 		glitch = canvas.glitch(texture);
-		
 		video.play();
-	});
+	}, false);
+	
+	video.addEventListener('ended', function() {
+		
+		if ($('#canvas video').size() > 0) {
+		    $('#canvas video').remove();
+		}
+		
+		video = null;
+		glitch = null;
+		
+		console.log("video ended");
+	}, false);
 };
 
 function animate() {
 	
 	setTimeout(animate, 1000 / fps);
 	
-	// update video glitching
-	if (videoCanvas) {
-		var videoContext2d = videoCanvas.getContext('2d');
-		videoContext2d.drawImage(video, 0, 0, videoCanvas.width, videoCanvas.height);
-		
-		var src = videoCanvas.toDataURL('image/jpeg');
-		
-		var image = new Image();
-		image.src = src;
-		loadImage(src);
+	
+	// update controller
+	var lo = glitchGui.get('minWidth');
+	var v = glitchGui.get('width');
+	var hi = glitchGui.get('maxWidth');
+	if (lo.getValue() > hi.getValue()) lo.setValue(hi.getValue());
+	if (lo.getValue() > v.getValue()) v.setValue(lo.getValue());
+	if (hi.getValue() < v.getValue()) v.setValue(hi.getValue());
+	if (glitchGui.get('animateWidth').getValue()) {
+		var range = (hi.getValue() - lo.getValue());
+		glitchParam.width = Math.cos(theta / Math.PI * 180) * range / 2 + range / 2 + lo.getValue();
 	}
 	
-	if (glitch)
-	{
-		var d = new Date();
+	var lo = glitchGui.get('minHeight');
+	var v = glitchGui.get('height');
+	var hi = glitchGui.get('maxHeight');
+	if (lo.getValue() > hi.getValue()) lo.setValue(hi.getValue());
+	if (lo.getValue() > v.getValue()) v.setValue(lo.getValue());
+	if (hi.getValue() < v.getValue()) v.setValue(hi.getValue());
+	if (glitchGui.get('animateHeight').getValue()) {
+		var range = (hi.getValue() - lo.getValue());
+		glitchParam.height = Math.cos(theta / Math.PI * 180) * range / 2 + range / 2 + lo.getValue();
+	}
+	
+	var lo = glitchGui.get('minOffset');
+	var v = glitchGui.get('offset');
+	var hi = glitchGui.get('maxOffset');
+	if (lo.getValue() > hi.getValue()) lo.setValue(hi.getValue());
+	if (lo.getValue() > v.getValue()) v.setValue(lo.getValue());
+	if (hi.getValue() < v.getValue()) v.setValue(hi.getValue());
+	if (glitchGui.get('animateOffset').getValue()) {
+		var range = (hi.getValue() - lo.getValue());
+		glitchParam.offset = Math.cos(theta / Math.PI * 180) * range / 2 + range / 2 + lo.getValue();
+	}
+	
+	// update video glitching
+	if (video) {
+		if (video.videoCanvas) {
+			
+			// in advance draw on temp canvas for getting data as URI
+			// using toDataURL method
+			var videoContext2d = video.videoCanvas.getContext('2d');
+			videoContext2d.drawImage(video, 0, 0, video.videoCanvas.width, video.videoCanvas.height);
+
+			// draw as image on canvas
+			loadImage(video.videoCanvas.toDataURL('image/jpeg'));
+		}
+	}
+	
+	if (glitch) {
+	    
+		// update glitch controller
+		for (var i in glitchGui.__controllers) {
+			glitchGui.__controllers[i].updateDisplay();
+		}
+		for (key in glitchGui.__folders) {
+			for (var i in glitchGui.__folders[key].__controllers) {
+				glitchGui.__folders[key].__controllers[i].updateDisplay();
+			}
+		}
 		
-		// auto glitch
-		//glitchParam.width = Math.cos(theta / Math.PI * 180);
-		//glitchParam.height = Math.sin(theta / Math.PI * 180) * 10 + 90;
-		//glitchParam.width = Math.cos(theta / Math.PI * 180) * 50 + 50;
-		//glitchParam.height = Math.sin(theta / Math.PI * 180) * 50 + 50;
-		
-		// Iterate over all controllers
-		// for (var i in ui.__controllers) {
-		// 	ui.__controllers[i].updateDisplay();
-		// }
-		
-		setGlitchFormat(ui.__controllers[1].getValue());
-		setGlitchType(ui.__controllers[2].getValue());
-		setGlitchWidth(ui.__controllers[3].getValue());
-		setGlitchHeight(ui.__controllers[4].getValue());
-		setGlitchOffset(ui.__controllers[5].getValue());
+		setGlitchFormat(glitchGui.get('format').getValue());
+		setGlitchType(glitchGui.get('type').getValue());
+		setGlitchWidth(glitchGui.get('width').getValue());
+		setGlitchHeight(glitchGui.get('height').getValue());
+		setGlitchOffset(glitchGui.get('offset').getValue());
 		glitch.update();
 		
-		theta++;
+		theta += .0012;
 		if (theta > 360) theta = 0;
 	}
 };
@@ -102,36 +180,42 @@ function animate() {
 function setGlitchFormat(v) {
 	
 	if (!glitch) return;
+	
 	glitch.format(parseInt(v));
 };
 
 function setGlitchType(v) {
 	
 	if (!glitch) return;
+	
 	glitch.type(parseInt(v));
 };
 
 function setGlitchWidth(v) {
 	
 	if (!glitch) return;
+	
 	glitch.shortenX(parseInt(texture._.width * (100 - v) / 100));
 };
 
 function setGlitchHeight(v) {
 	
 	if (!glitch) return;
+	
 	glitch.shortenY(parseInt(texture._.height * (100 - v) / 100));
 };
 
 function setGlitchOffset(v) {
 	
 	if (!glitch) return;
+	
 	glitch.offset(parseInt(v));
 };
 
 function saveImage() {
 	
 	if (!canvas) return;
+	
 	try {
 		window.open(canvas.toDataURL("image/jpeg"));
 	} catch (e) {
@@ -139,19 +223,41 @@ function saveImage() {
 	}
 };
 
+function GlitchParams() {
+    
+    var gl = canvas._.gl;
+	this.title = 'This is WebGL Glitch';
+	this.format = gl.RGBA;
+	this.type = gl.UNSIGNED_BYTE;
+	
+	this.minWidth = 0;
+	this.maxWidth = 100;
+	this.width = 100;
+	this.animateWidth = false;
+	
+	this.minHeight = 0;
+	this.maxHeight = 100;
+	this.height = 100;
+	this.animateHeight = false;
+	
+	this.minOffset = 0;
+	this.maxOffset = 100;
+	this.offset = 0;
+	this.animateOffset = false;
+	
+	this.saveImage = saveImage;
+};
+
 window.onload = function() {
 	
 	try {
 		canvas = fx.canvas();
-		
 	} catch (e) {
-		
 		alert('Sorry, but this browser doesn\'t support WebGL.');
 		return;
 	}
 	
 	if (typeof(FileReader) == "undefined") {
-		
 		alert('Sorry, but this browser doesn\'t support FileReader.');
 		return;
 	}
@@ -159,44 +265,53 @@ window.onload = function() {
 	// add canvas to stage
 	$('#canvas canvas').remove();
 	$('#canvas').append($(canvas));
+	$('#canvas canvas').hide();
+	
+	// add glitch gui
+	glitchGui = new dat.GUI({ autoPlace: false, width: 320 });
+	glitchParam = new GlitchParams();
 	
 	var gl = canvas._.gl;
-	
-	////////////////////////////////////////////////////////////////////////////////
-	// SET GLITCH GUI
-	////////////////////////////////////////////////////////////////////////////////
-	
-	function Parameter() {
-		this.title = 'This is WebGL Glitch';
-		this.format = gl.RGBA;
-		this.type = gl.UNSIGNED_BYTE;
-		this.width = 100;
-		this.height = 100;
-		this.offset = 0;
-		this.saveImage = saveImage;
-	};
-	
-	ui = new dat.GUI({ autoPlace: false, width: 320 });
-	glitchParam = new Parameter();
 	var format = {'ALPHA':gl.ALPHA, 'LUMINANCE':gl.LUMINANCE, 'LUMINANCE_ALPHA':gl.LUMINANCE_ALPHA, 'RGB':gl.RGB, 'RGBA':gl.RGBA};
-	var type = {'UNSIGNED_BYTE':gl.UNSIGNED_BYTE, 'UNSIGNED_SHORT_5_6_5':gl.UNSIGNED_SHORT_5_6_5, 'UNSIGNED_SHORT_4_4_4_4':gl.UNSIGNED_SHORT_4_4_4_4, 'UNSIGNED_SHORT_5_5_5_1':gl.UNSIGNED_SHORT_5_5_5_1};
 	
-	ui.add(glitchParam, 'title');
-	ui.add(glitchParam, 'format', format).onChange(setGlitchFormat);
-	ui.add(glitchParam, 'type', type).onChange(setGlitchType);
-	ui.add(glitchParam, 'width', 0, 100, .1).onChange(setGlitchWidth);
-	ui.add(glitchParam, 'height', 0, 100, .1).onChange(setGlitchHeight);
-	ui.add(glitchParam, 'offset', 0, 100, .1).onChange(setGlitchOffset);
-	ui.add(glitchParam, 'saveImage').name('save image');
+	// only UNSIGNED_BYTE value is effective as glitch. I can't understand why is is.
+	//var type = {'UNSIGNED_BYTE':gl.UNSIGNED_BYTE, 'UNSIGNED_SHORT_5_6_5':gl.UNSIGNED_SHORT_5_6_5, 'UNSIGNED_SHORT_4_4_4_4':gl.UNSIGNED_SHORT_4_4_4_4, 'UNSIGNED_SHORT_5_5_5_1':gl.UNSIGNED_SHORT_5_5_5_1};
+	var type = { 'UNSIGNED_BYTE':gl.UNSIGNED_BYTE };
 	
-	$(ui.domElement).find('input[type=text]:first').attr('readonly', 'true');
 	
+	glitchGui.add(glitchParam, 'title');
+	glitchGui.add(glitchParam, 'format', format).onChange(setGlitchFormat);
+	glitchGui.add(glitchParam, 'type', type).onChange(setGlitchType);
+	var f1 = glitchGui.addFolder('width');
+	f1.add(glitchParam, 'minWidth').min(0).max(100).step(.1).name('min');
+	f1.add(glitchParam, 'maxWidth').min(0).max(100).step(.1).name('max');
+	f1.add(glitchParam, 'width').min(0).max(100).step(.1).onChange(setGlitchWidth);
+	f1.add(glitchParam, 'animateWidth').name('animate');
+	f1.open();
+	var f2 = glitchGui.addFolder('height');
+	f2.add(glitchParam, 'minHeight').min(0).max(100).step(.1).name('min');
+	f2.add(glitchParam, 'maxHeight').min(0).max(100).step(.1).name('max');
+	f2.add(glitchParam, 'height').min(0).max(100).step(.1).onChange(setGlitchHeight);
+	f2.add(glitchParam, 'animateHeight').name('animate');
+	f2.open();
+	var f3 = glitchGui.addFolder('offset');
+	f3.add(glitchParam, 'minOffset').min(0).max(100).step(.1).name('min');
+	f3.add(glitchParam, 'maxOffset').min(0).max(100).step(.1).name('max');
+	f3.add(glitchParam, 'offset').min(0).max(100).step(.1).onChange(setGlitchOffset);
+	f3.add(glitchParam, 'animateOffset').name('animate');
+	f3.open();
+	glitchGui.add(glitchParam, 'saveImage').name('save image');
+	
+	// set readonly attribute to gui named 'title' because title is app name.
+	$(glitchGui.domElement).find('input[type=text]:first').attr('readonly', 'true');
+	
+	// add to stage
 	var container = document.getElementById('gui-container');
-	container.appendChild(ui.domElement);
+	container.appendChild(glitchGui.domElement);
 	
 	
 	////////////////////////////////////////////////////////////////////////////////
-	// SET FILE LISTENER
+	// SET DROP FILE LISTENER
 	////////////////////////////////////////////////////////////////////////////////
 	
 	window.addEventListener('dragover', function(e) {
@@ -208,10 +323,6 @@ window.onload = function() {
 
 	window.addEventListener('drop', function(e) {
 		
-		// initialize
-		init();
-		
-		
 		// off default blower action
 		e.preventDefault();
 		
@@ -221,6 +332,9 @@ window.onload = function() {
 		
 		if (fileType.match(/image\/\w+/)) {
 			
+			// initialize
+			init();
+			
 			reader = new FileReader();
 			reader.onload = function(e) {
 				loadImage(e.target.result);
@@ -228,6 +342,9 @@ window.onload = function() {
 			reader.readAsDataURL(file);
 			
 		} else if (fileType.match(/video\/\w+/)) {
+			
+			// initialize
+			init();
 			
 			reader = new FileReader();
 			reader.onload = function(e) {
